4770ba705eba720de680a0c617d50e06803e0d4b
Dobre Bogdan <dobrebogdan98@gmail.com>
2020-12-02 19:56:27 +0200

    Finished chapters 2-5

--- InfractionalNetworkAnalisys.R
+++ InfractionalNetworkAnalisys.R
@@ -1,3 +1,7 @@
+library(network)
+library(statnet)
+library(intergraph)
+library(RColorBrewer)
 netmat <- rbind(c(1,2),
                 c(1,3),
                 c(2,3),
@@ -36,49 +40,49 @@ netmat <- rbind(c(1,2),
 
 net <- network(netmat, matrix.type="edgelist")
 netmatsym <- symmetrize(as.sociomatrix(net), rule ="weak")
-                        
+
 
 netsym <- network(netmatsym, matrix.type="adjacency")
 network.vertex.names(netsym) <- c("Berescu Liliana", 
-                               "Berescu Angelus", 
-                               "Berescu Cristinel",
-                               "Bighiu George",
-                               "Mantu Mariana",
-                               "Mantu Iordache",
-                               "Taga Filip",
-                               "Taga Gheorghe",
-                               "Saim Angela",
-                               "Galca Gheorghe",
-                               "Chelaru Ioan",
-                               "Melciu Leonardo",
-                               "Dura Dana",
-                               "Dura Costel",
-                               "Nedelcu Petru",
-                               "Nastase Toader",
-                               "Stoian Constatin",
-                               "Olaru Andrei",
-                               "Dura Ionel",
-                               "Palici Vasile",
-                               "Dragomir Ramona")
+                                  "Berescu Angelus", 
+                                  "Berescu Cristinel",
+                                  "Bighiu George",
+                                  "Mantu Mariana",
+                                  "Mantu Iordache",
+                                  "Taga Filip",
+                                  "Taga Gheorghe",
+                                  "Saim Angela",
+                                  "Galca Gheorghe",
+                                  "Chelaru Ioan",
+                                  "Melciu Leonardo",
+                                  "Dura Dana",
+                                  "Dura Costel",
+                                  "Nedelcu Petru",
+                                  "Nastase Toader",
+                                  "Stoian Constatin",
+                                  "Olaru Andrei",
+                                  "Dura Ionel",
+                                  "Palici Vasile",
+                                  "Dragomir Ramona")
 set.vertex.attribute(netsym, "role", c("C", 
-                                    "C", 
-                                    "C",
-                                    "CR",
-                                    "C",
-                                    "C",
-                                    "CT",
-                                    "CT",
-                                    "CT",
-                                    "C",
-                                    "A",
-                                    "A",
-                                    "C",
-                                    "C",
-                                    "C",
-                                    "C",
-                                    "CT",
-                                    "D",
-                                    "D"))
+                                       "C", 
+                                       "C",
+                                       "CR",
+                                       "C",
+                                       "C",
+                                       "CT",
+                                       "CT",
+                                       "CT",
+                                       "C",
+                                       "A",
+                                       "A",
+                                       "C",
+                                       "C",
+                                       "C",
+                                       "C",
+                                       "CT",
+                                       "D",
+                                       "D"))
 # C : Comerciant, CR : Cartita, CT: contrabandist, A: aducator clienti, D: depozitare
 netsym %v% "alldeg" <- degree(netsym)
 summary(netsym)
@@ -87,6 +91,7 @@ rolelab <- get.vertex.attribute(netsym, "role")
 my_pal <- brewer.pal(5,"Dark2")
 rolecat <- as.factor(get.vertex.attribute(netsym,"role"))
 plot(netsym,
+     main = "Infractional network",
      usearrows=FALSE, 
      mode="fruchtermanreingold", 
      vertex.col = my_pal[rolecat],
@@ -94,3 +99,92 @@ plot(netsym,
      displaylabels=T,
      vertex.cex = 1.5)
 
+
+# Capitolul 2 
+print("BASIC CHARACTERISTICS")
+
+print("Size:")
+print(network.size(netsym))
+
+print("Density:")
+print(gden(netsym))
+
+print("Components:")
+print(components(netsym))
+
+print("Diameter:")
+gd <- geodist(netsym)
+print(max(gd$gdist))
+
+print("Transitivity:")
+print(gtrans(netsym, mode="graph"))
+
+# Capitolul 3
+print("DIFFERENT REPRESENTATIONS")
+
+print("Sociomatrix:")
+print(as.sociomatrix(netsym))
+
+print("Edge list:")
+print(as.matrix(netsym, matrix.type="edgelist"))
+
+
+
+print("Filtering networks")
+netsymigraph <- asIgraph(netsym)
+print(get.vertex.attribute(netsym, "role"))
+comercianti <- get.inducedSubgraph(netsym, which (netsym %v% "role"=="C"))
+gplot(comercianti,displaylabels=TRUE, main="Comercianti")
+delete.vertices(comercianti, isolates(comercianti), main="Retelele de comercianti")
+gplot(comercianti, displaylabels = TRUE)
+
+# Capitolul 4
+print("NETWORK VISUALISATION")
+gplot(netsym,gmode="graph",edge.col="grey75",displaylabels=T,
+      vertex.cex=1.5,mode='circle',main="circle")
+gplot(netsym,gmode="graph",edge.col="grey75",displaylabels=T,
+      vertex.cex=1.5,mode='eigen',main="eigen")
+gplot(netsym,gmode="graph",edge.col="grey75",displaylabels=T,
+      vertex.cex=1.5,mode='random',main="random")
+gplot(netsym,gmode="graph",edge.col="grey75",displaylabels=T,
+      vertex.cex=1.5,mode='spring',main="spring")
+gplot(netsym,gmode="graph",edge.col="grey75",displaylabels=T,
+      vertex.cex=1.5,mode='fruchtermanreingold',main='fruchtermanreingold')
+gplot(netsym,gmode="graph",edge.col="grey75",displaylabels=T,
+      vertex.cex=1.5,mode='kamadakawai',
+      main='kamadakawai')
+
+# Capitolul 5
+plot(netsym,vertex.cex=0.5,main="Too small nodes")
+plot(netsym,vertex.cex=6,main="Too large nodes")
+plot(netsym,vertex.cex=2,main="Just right node size")
+
+sidenum <- 3:7
+plot(netsym,usearrows=FALSE,vertex.cex=4,main="Different node shapes",
+     displaylabels=F,vertex.sides=sidenum[rolecat])
+
+n_edge <- network.edgecount(netsym)
+linecol_pal <- c("blue","red","green")
+edge_cat <- sample(1:3,n_edge,replace=T)
+plot(netsym,vertex.cex=1.5,vertex.col="grey25", main="Edge coloring example",
+     edge.col=linecol_pal[edge_cat],edge.lwd=2)
+
+widths <- c(2,6,10)
+plot(netsym,vertex.cex=1.5,main="Different edge width",
+     edge.lwd=1.5*widths)
+
+n_edge <- network.edgecount(netsym)
+edge_cat <- sample(1:3,n_edge,replace=T)
+line_pal <- c(2,3,4)
+gplot(netsym,vertex.cex=0.8,gmode="graph", main="Different edge type",
+      vertex.col="gray50",edge.lwd=1.5,
+      edge.lty=line_pal[edge_cat])
+
+
+my_pal <- brewer.pal(5,"Dark2")
+rolecat <- as.factor(get.vertex.attribute(netsym,"role"))
+plot(netsym,
+     vertex.col=my_pal[rolecat])
+legend("bottomleft",legend=c("C","CT","CR","A","D"),
+       col=my_pal,pch=19,pt.cex=1.5,bty="n",
+       title="Criminal Role")
